724ff391be1179d5a29237e5fe6021f0687f35af
emreguler <emre_guler01@hotmail.com>
2021-05-26 23:37:10 +0300

    APL-53 - DONE

--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -1,21 +1,48 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Route } from "react-router";
-import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Context from './context';
-import Home  from "./pages/Home/Home";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import HttpApi from "i18next-http-backend";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Context from "./context";
+import Home from "./pages/Home/Home";
 
 const rootElement = document.getElementById("root");
+
+i18n
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    fallbackLng: "en",
+    detection: {
+      order: ["cookie", "htmlTag", "localStorage", "path", "subdomain"],
+      caches: ["cookie"],
+    },
+    interpolation: {
+      escapeValue: false,
+    },
+    backend: {
+      loadPath: "/assets/locals/{{lng}}.json",
+    },
+    react: {
+      useSuspense: false,
+    },
+  });
 
 const App = () => {
   return (
     <BrowserRouter>
       <Route path="/" component={Home} />
     </BrowserRouter>
-  )
-}
+  );
+};
 
 ReactDOM.render(
   <Context>
--- ClientApp/src/pages/Home/Components/Header.js
+++ ClientApp/src/pages/Home/Components/Header.js
@@ -1,24 +1,32 @@
 import React from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
-
-import { AiOutlineUserAdd } from 'react-icons/ai';
+import { useTranslation } from "react-i18next";
+import { AiOutlineUserAdd } from "react-icons/ai";
 import "../assets/header.scss";
 
 const Header = () => {
+  const { t } = useTranslation();
   return (
     <header>
       <Container className="container">
-          <Row>
-            <Col>
-                <img src={require('../assets/apolloLogo.png')} alt="Apollo's Logo" />
-            </Col>
-            <Col>
-              <div className="navElementContainer">
-                <Button variant="light" ><AiOutlineUserAdd / > Kayıt Ol (Takım)</Button>
-                <Button variant="light" ><AiOutlineUserAdd / > Kayıt Ol (Oyuncu)</Button>
-              </div>
-            </Col>
-          </Row>
+        <Row>
+          <Col>
+            <img
+              src={require("../assets/apolloLogo.png")}
+              alt="Apollo's Logo"
+            />
+          </Col>
+          <Col>
+            <div className="navElementContainer">
+              <Button variant="light">
+                <AiOutlineUserAdd /> Kayıt Ol (Takım)
+              </Button>
+              <Button variant="light">
+                <AiOutlineUserAdd /> {t("RegisterAsUser")}
+              </Button>
+            </div>
+          </Col>
+        </Row>
       </Container>
     </header>
   );
