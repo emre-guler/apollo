ca5065a00314b063b73c6a995eae0e5d2687c3eb
emreguler <emre_guler01@hotmail.com>
2021-05-29 18:03:30 +0300

    Content progress.

--- ClientApp/src/pages/Home/Components/Content.js
+++ ClientApp/src/pages/Home/Components/Content.js
@@ -1,28 +1,54 @@
 import React, { useState } from "react";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
+import Typed from "react-typed";
+import AwesomeSlider from "react-awesome-slider";
+import withAutoplay from "react-awesome-slider/dist/autoplay";
 import "./assets/Content.scss";
+import "react-awesome-slider/dist/styles.css";
 
 const Content = () => {
   const { t } = useTranslation();
-  const [words, setWords] = useState(["CS:GO", "Valorant", "LOL"]);
-  const [datePeriod, setDatePeriod] = useState(2000);
+  const AutoplaySlider = withAutoplay(AwesomeSlider);
+  const [words] = useState(["CS:GO", "Valorant", "LOL"]);
   return (
-    <section>
-      <Container>
-        <Row className={"contentContainer"}>
-          <Col xs={12} md={12} xl={6}>
-            <div className={"mainContentContainer"}>
-              <h1>{t("HeyYou")}</h1>
-              <h1 className={"gameName"}>CSGO</h1>
-            </div>
-          </Col>
-          <Col xs={12} md={12} xl={6}>
-            asd
-          </Col>
-        </Row>
-      </Container>
-    </section>
+    <>
+      <section>
+        <Container>
+          <Row className={"contentContainer"}>
+            <Col xs={12} md={12} xl={6}>
+              <div className={"mainContentContainer"}>
+                <h1>{t("HeyYou")}</h1>
+                <Typed
+                  strings={words}
+                  typeSpeed={100}
+                  backSpeed={100}
+                  loop
+                  className={"gameName"}
+                />
+                <h1 className={"altMessage"}>{t("TeamsAreHere")}</h1>
+                <h1 className={"altMessage"}>{t("WannaToJoinUs")}</h1>
+              </div>
+            </Col>
+            <Col xs={12} md={12} xl={6}>
+              <div className={"bannerContentContainer"}>
+                <AutoplaySlider
+                  play={true}
+                  cancelOnInteraction={false}
+                  organicArrows={false}
+                  interval={2000}
+                  bullets={false}
+                >
+                  <div data-src="/assets/teamLogosCSGO.png" />
+                  <div data-src="/assets/teamLogosLOL.png" />
+                  <div data-src="/assets/teamLogosValorant.png" />
+                </AutoplaySlider>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </>
   );
 };
 
