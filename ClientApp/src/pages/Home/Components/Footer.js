d52a9476ad9eeb72924346d89213e03eaff71e9a
emreguler <emre_guler01@hotmail.com>
2021-05-30 15:55:32 +0300

    footer progress

--- ClientApp/src/pages/Home/Components/Footer.js
+++ ClientApp/src/pages/Home/Components/Footer.js
@@ -1,22 +1,31 @@
 import React from "react";
-import { Container, Row, Col } from 'react-bootstrap';
-import { useTranslation } from 'react-i18next';
-import './assets/Footer.scss'; 
+import { Container, Row, Col } from "react-bootstrap";
+import { useTranslation } from "react-i18next";
+import "./assets/Footer.scss";
 
 const Footer = () => {
+  const { t } = useTranslation();
   return (
     <>
       <footer>
-          <Container>
-              <Row>
-                  <Col xs={12} md={5} xl={5}>
-                      <div className={"footerLeftContainer"}>
-                          <img src={""} />
-                      </div>
-                  </Col>
-                  <Col xs={12} md={7} xl={7}></Col>
-              </Row>
-          </Container>
+        <Container className={"footerContainer"}>
+          <Row>
+            <Col xs={12} md={5} xl={5}>
+              <div className={"footerLeftContainer"}>
+                <img src={require("./assets/apolloLogo.png")} />
+                <p>{t("FooterText")}</p>
+              </div>
+            </Col>
+            <Col xs={12} md={7} xl={7}>
+                <Container>
+                    <Row>
+                        <Col ></Col>
+                        <Col></Col>
+                    </Row>
+                </Container>
+            </Col>
+          </Row>
+        </Container>
       </footer>
     </>
   );
